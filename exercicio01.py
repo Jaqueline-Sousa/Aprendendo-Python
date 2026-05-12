d5edb908f5b230d77ecafe5ae04a6df3b3fadae0
Jaqueline-Sousa <senhoritastark9868@gmail.com>
2026-05-12 17:44:30 -0300

    Meus primeiros codigos

---
+++ exercicio01.py
@@ -0,0 +1,22 @@
+N1 = int (input ("Digite o primeiro número: N "))
+N2 = int (input ("Digite o segundo número: N "))
+Resultado = N1 // N2
+Resultado2 = N1 % N2
+Resultado3 = N1 ** N2
+print ("O resultado da parte inteira da divisão é: ", Resultado)
+print ("O resultado2 é o resto da divisão: ", Resultado2)
+print ("O resultado3 da potencia é: ", Resultado3)
+
+print ("---------------------------------")
+print (" Operadores Relacionados ")
+print ("------------------------------------")
+
+Relação1 = N1 > N2
+Relação2 = N1 < N2
+Relação3 = N1 >= N2
+Relação4 = N1 <= N2
+Relação5 = N1 == N2
+Relação6 = N1 != N2
+
+
+print ("Os resultados das relações estão a baixo  \n{} \n{} \n{} \n{} \n{} \n{}".format (Relação1, Relação2,Relação3,Relação4, Relação5, Relação6))
---
+++ programa01.py
@@ -0,0 +1,20 @@
+print ("Hello World!")
+print ("No mundo ninja, aqueles que quebram as regras são lixo, mas aqueles que abandona seus amigos são piores que lixo")
+print ("1") #str (String de caracteres)
+print (1)   #int (Número inteiro)
+print (-2) #int (Número inteiro)
+print (8.2) #float (Número real ou decimal, ponto flutuante)
+print (True) #Verdadeiro (booleano - bool)
+print (False) #falso (booleano - bool)
+
+nome = "Janaia"
+print (nome)
+idade = 17
+peso = 45.0
+maioridade = false
+
+print ("Essa é a ",nome,", ela tem ",idade," anos e pesa ",peso," kg")
+
+print ("O nome dela é" + nome+ "eu encontrei ela no Tínder")
+
+print ("Hoje é o aniversário da {} e ela está completando {} anos e pesa {} kg alem disso ela é maior de idade:" format (nome,idade, peso, maioridade))
