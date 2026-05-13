d1f347f629c25353b3af4c4965e69d80374dd31b
Jaqueline-Sousa <senhoritastark9868@gmail.com>
2026-05-13 17:37:17 -0300

    Aula 2 Python

--- exercicio01.py
+++ exercicio01.py
@@ -1,7 +1,7 @@
 N1 = int (input ("Digite o primeiro número: N "))
 N2 = int (input ("Digite o segundo número: N "))
-Resultado = N1 // N2
-Resultado2 = N1 % N2
+Resultado = N1 // N2  
+Resultado2 = N1 % N2  
 Resultado3 = N1 ** N2
 print ("O resultado da parte inteira da divisão é: ", Resultado)
 print ("O resultado2 é o resto da divisão: ", Resultado2)
